dbb186502471d75d19cc29264951bccc1c5281d6
Bhavana <bhavanar029@gmail.com>
2024-11-01 11:40:30 +0530

    cards

--- counting-cards.js
+++ counting-cards.js
@@ -29,5 +29,17 @@ function cc(card) {
   // Only change code above this line
 }
 
- const y = cc(2); cc(3); cc(7); cc('K'); cc('A');
+ const y = cc(2); //cc(3); cc(7); cc('K'); cc('A');
  console.log(y);
+
+ const a = cc(3);
+ console.log(a);
+
+ const b = cc(7);
+ console.log(b);
+
+ const c = cc('k');
+ console.log(c);
+
+ const d = cc('A');
+ console.log(d);
