70df9e35d58866cfd489d42f328367fd56e66d43
Bhavana <bhavanar029@gmail.com>
2024-11-01 12:22:19 +0530

    objects

---
+++ javascript-object.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+   "name" : "abc",
+   "legs" : 4,
+   "tails" : 1,
+   "friends" : ["xy" , "mn"]
+    // Only change code above this line
+  }
+  console.log(myDog);
