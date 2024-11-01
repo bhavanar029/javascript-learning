f70a28a85047b81f468230d00add3d501ad710ab
Bhavana <bhavanar029@gmail.com>
2024-11-01 12:24:04 +0530

    nested-obj

---
+++ nested-obj.js
@@ -0,0 +1,16 @@
+//accessing nested objects
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
+  console.log(myStorage);
+  
