c55034c1e1887a08347f7b1230b491bab95310b2
Bhavana <bhavanar029@gmail.com>
2024-11-08 15:45:20 +0530

    reverse-string

---
+++ reverse-string.js
@@ -0,0 +1,8 @@
+//convert string to array, reverse the array, then convert array back to string
+//first split string, reverse string, join string
+
+const reverseString = (str) => str.split("").reverse().join("")
+console.log(reverseString("hello"));
+console.log(reverseString("Bhavana"));
+console.log(reverseString("Kavya"));
+
